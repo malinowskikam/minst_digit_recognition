36454d7f3d4956e357a8db087be3e4fd18d00d56
malinowskikam <malinowskikam96@gmail.com>
2020-01-22 05:16:23 +0100

    model generation in main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import nb
-from data import get_training_data_2d
+import cnn, rf, svm, nn5, dt, nb
+from data import get_training_data_2d, get_training_data_4d
+
+data = get_training_data_2d()
 
 model = nb.create_model()
-data = get_training_data_2d()
-nb.fit(model,data)
-print(nb.evaluate_acc(model,data))
+nb.fit(model, data)
+
+model = dt.create_model()
+dt.fit(model, data)
+
+model = nn5.create_model()
+nn5.fit(model, data)
+
+model = svm.create_model()
+svm.fit(model, data)
+
+model = rf.create_model()
+dt.fit(model, data)
+
+
+data = get_training_data_4d()
+
+model = cnn.create_model()
+cnn.fit(model, data)
