083fa3cf0043b25d63a758f8dff45bf7e979ce82
Kamil Malinowski <malinowskikam96@gmail.com>
2024-11-17 23:26:41 +0100

    Added main script

---
+++ minst_digit_recognition/__init__.py
@@ -0,0 +1,3 @@
+"""
+main minst_digit_recognition package
+"""
--- minst_digit_recognition/main.py
+++ minst_digit_recognition/main.py
@@ -1,25 +1,15 @@
-from minst_digit_recognition import cnn, rf, svm, nn5, dt, nb
-from minst_digit_recognition.data import get_training_data_2d, get_training_data_4d
-
-data = get_training_data_2d()
-
-model = nb.create_model()
-nb.fit(model, data)
-
-model = dt.create_model()
-dt.fit(model, data)
-
-model = nn5.create_model()
-nn5.fit(model, data)
-
-model = svm.create_model()
-svm.fit(model, data)
-
-model = rf.create_model()
-dt.fit(model, data)
+"""
+main executable script
+executes fir, score and confusion_matrix scripts in sequence
+"""
 
 
-data = get_training_data_4d()
+def main():
+    from minst_digit_recognition import fit, score, confusion_matrix
+    fit.main()
+    score.main()
+    confusion_matrix.main()
 
-model = cnn.create_model()
-cnn.fit(model, data)
+
+if __name__ == "__main__":
+    main()
