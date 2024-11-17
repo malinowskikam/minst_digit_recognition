a3a2981e06042b60b6ffd41b8610ce05acd1e5fc
Kamil Malinowski <malinowskikam96@gmail.com>
2024-11-17 23:20:01 +0100

    Updated data package

---
+++ minst_digit_recognition/data/trainingData.py
@@ -0,0 +1,36 @@
+import pathlib
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+class TrainingData:
+    """
+    Data loader for ML models
+    """
+    train_x: np.ndarray
+    train_y: np.ndarray
+    test_x: np.ndarray
+    test_y: np.ndarray
+
+    def __init__(self, data_dir="data", data_file="train.csv", test_size=0.1):
+        self._data_dir = data_dir
+        self._data_file = data_file
+        self._test_size = test_size
+
+        file_ext = data_file.rsplit(".", 1)[-1]
+        match file_ext:
+            case "csv":
+                (self.train_x, self.test_x, self.train_y, self.test_y) = self._load_data_csv()
+            case _:
+                raise AttributeError(f"Unknown file extension: {file_ext}")
+
+    def _load_data_csv(self) -> list:
+        train_data_path = pathlib.Path(self._data_dir, self._data_file)
+        dataframe = pd.read_csv(train_data_path).apply(pd.to_numeric)
+
+        x = dataframe.drop("label", axis=1).values
+        y = dataframe["label"].values
+
+        return train_test_split(x, y, test_size=self._test_size, random_state=1234)
