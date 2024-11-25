d6a1cc00c8c905f45c6e7e23e547731a040f8eb5
Kamil Malinowski <malinowskikam96@gmail.com>
2024-11-25 16:55:11 +0100

    Added main layout, added canvas to main layout

--- mnist_digit_recognition/app/components/__init__.py
+++ mnist_digit_recognition/app/components/__init__.py
@@ -1,1 +1,2 @@
 from mnist_digit_recognition.app.components.model_picker import ModelPicker
+from mnist_digit_recognition.app.components.canvas import Canvas
--- mnist_digit_recognition/app/main_window.py
+++ mnist_digit_recognition/app/main_window.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import QMainWindow, QLabel, QWidget, QHBoxLayout
 
-from mnist_digit_recognition.app.components import ModelPicker
+from mnist_digit_recognition.app.components import ModelPicker, Canvas
 from mnist_digit_recognition.models.base import MlModel
 
 
@@ -13,15 +13,24 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle(self.title)
 
-        self.model_picker = ModelPicker()
+        main_layout = QHBoxLayout()
+        main_layout.setContentsMargins(20,20,20,20)
+        main_layout.setSpacing(20)
 
-        model_label = QLabel("Model: ")
+        self.canvas = Canvas()
+        main_layout.addWidget(self.canvas)
 
         model_layout = QHBoxLayout()
+
+        model_label = QLabel("Model: ")
         model_layout.addWidget(model_label)
+
+        self.model_picker = ModelPicker()
         model_layout.addWidget(self.model_picker)
+
+        main_layout.addLayout(model_layout)
 
         # Set the layout
         w = QWidget()
-        w.setLayout(model_layout)
+        w.setLayout(main_layout)
         self.setCentralWidget(w)
